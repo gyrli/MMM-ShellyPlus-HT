d82138c51dd05e5f976dd6d30c9b2ca31e7ebbf4
IoT-Coder <146182054+IoT-Coder@users.noreply.github.com>
2023-09-27 14:01:34 +0200

    Update config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -9,9 +9,9 @@
 				ShellyHTApiPath_t: "http://192.168.178.126/rpc/Temperature.GetStatus?id=0",
 				ShellyHTApiPath_h: "http://192.168.178.126/rpc/Humidity.GetStatus?id=0",
 				ShellyHTApiPath_b: "http://192.168.178.126/rpc/DevicePower.GetStatus?id=0",
-				RefreshInterval: "5000", //milliseconds
-				displayUpdated: "true",
-				horizontalView: "false"
+				RefreshInterval: 5000, //milliseconds
+				displayUpdated: true,
+				horizontalView: false
 
 			}
 		},
